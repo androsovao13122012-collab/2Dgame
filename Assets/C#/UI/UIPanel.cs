b28aa9ad7232a1de96651f2ea637f57103ad140a
androsovao13122012-collab <androsovao13122012@gmail.com>
2025-09-16 20:32:25 +0300

    Panel

--- Assets/C#/UI/UIPanel.cs
+++ Assets/C#/UI/UIPanel.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject panel;
+    public bool isActivePanel;
+    private void Start()
     {
-        
+        panel.SetActive(false);
+        Time.timeScale = 1;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isActivePanel = !isActivePanel;
+            panel.SetActive(isActivePanel);
+            Time.timeScale = isActivePanel?0:1;
+        }
+    }
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        panel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
 }
